4511bdb4763ff9cb316db831a0e47fa20d7eb700
Abhinav Bansal <abhinavbansal112211@gmail.com>
2025-05-20 23:34:17 +0530

    update

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
+
+app.use(cors());
+
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
@@ -11,6 +16,11 @@ mongoose.connect(process.env.MONGO_URI)
   .catch((err) => console.log(err));
 
 app.use('/api/users', userRoutes);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
